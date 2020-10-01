645adb705b2603f6392bc33e307a6b5d8b54d1fe
Gaurav parmar <54779403+thegauravparmar@users.noreply.github.com>
2020-10-02 00:39:57 +0530

    python script added

---
+++ sample.py
@@ -0,0 +1,93 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
+from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
+from telethon.tl.functions.channels import InviteToChannelRequest
+import sys
+import csv
+import traceback
+import time
+
+SLEEP_TIME = 30
+
+api_id = 857215
+api_hash = 'be8f90cb661b0e19b233483b7f1b64cf'
+phone = '+917742862844'
+client = TelegramClient(phone, api_id, api_hash)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
+
+input_file = sys.argv[1]
+users = []
+with open(input_file, encoding='UTF-8') as f:
+    rows = csv.reader(f,delimiter=",",lineterminator="\n")
+    next(rows, None)
+    for row in rows:
+        user = {}
+        user['username'] = row[0]
+        user['id'] = int(row[1])
+        user['access_hash'] = int(row[2])
+        user['name'] = row[3]
+        users.append(user)
+
+chats = []
+last_date = None
+chunk_size = 200
+groups=[]
+
+result = client(GetDialogsRequest(
+             offset_date=last_date,
+             offset_id=0,
+             offset_peer=InputPeerEmpty(),
+             limit=chunk_size,
+             hash = 0
+         ))
+chats.extend(result.chats)
+
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
+
+print('Choose a group to add members:')
+i=0
+for group in groups:
+    print(str(i) + '- ' + group.title)
+    i+=1
+
+g_index = input("Enter a Number: ")
+target_group=groups[int(g_index)]
+
+target_group_entity = client.get_entity('https://telegram.me/joinchat/AAAAAFWfem5fXiZY-nV9TA')
+
+mode = int(input("Enter 1 to add by username or 2 to add by ID: "))
+
+for user in users:
+    try:
+        print ("Adding {}".format(user['id']))
+        if mode == 1:
+            if user['username'] == "":
+                continue
+            user_to_add = client.get_input_entity(user['username'])
+        elif mode == 2:
+            user_to_add = InputPeerUser(user['id'], user['access_hash'])
+        else:
+            sys.exit("Invalid Mode Selected. Please Try Again.")
+        client(InviteToChannelRequest(target_group_entity,[user_to_add]))
+    except PeerFloodError:
+        print("Getting Flood Error from telegram.")
+        print("Waiting {} seconds".format(SLEEP_TIME))
+        time.sleep(SLEEP_TIME)
+    except UserPrivacyRestrictedError:
+        print("The user's privacy settings do not allow you to do this. Skipping.")
+    except:
+        traceback.print_exc()
+        print("Unexpected Error")
+        continue
+
+        
---
+++ test.py
@@ -0,0 +1,63 @@
+from telethon.sync import TelegramClient
+from telethon.tl.types import InputPeerUser
+from telethon.errors.rpcerrorlist import PeerFloodError
+import sys
+import csv
+import random
+import time
+api_id = 857215
+api_hash = 'be8f90cb661b0e19b233483b7f1b64cf'
+phone = '+917742862844'
+
+SLEEP_TIME = 3
+client = TelegramClient(phone, api_id, api_hash)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
+
+input_file = sys.argv[1]
+users = []
+with open(input_file, encoding='UTF-8') as f:
+    rows = csv.reader(f,delimiter=",",lineterminator="\n")
+    next(rows, None)
+    for row in rows:
+        user = {}
+        user['username'] = row[0]
+        user['id'] = int(row[1])
+        user['access_hash'] = int(row[2])
+        user['name'] = row[3]
+        users.append(user)
+
+mode = int(input("Enter 1 to send by user ID or 2 to send by username: "))
+
+messages= ["Hey {}, do you want to download latest movies? Join us on https://t.me/joinchat/Lx8h2UnxG7Ndt5oS8GRGoA"]
+
+for user in users:
+    if mode == 2:
+        if user['username'] == "":
+            continue
+        receiver = client.get_input_entity(user['username'])
+    elif mode == 1:
+        receiver = InputPeerUser(user['id'],user['access_hash'])
+    else:
+        print("Invalid Mode. Exiting.")
+        client.disconnect()
+        sys.exit()
+    message = random.choice(messages)
+    try:
+        print("Sending Message to:", user['name'])
+        client.send_message(receiver, message.format(user['name']))
+        print("Waiting {} seconds".format(SLEEP_TIME))
+        time.sleep(SLEEP_TIME)
+    except PeerFloodError:
+        print("Getting Flood Error from telegram. Script is stopping now. Please try again after some time.")
+        client.disconnect()
+        sys.exit()
+    except Exception as e:
+        print("Error:", e)
+        print("Trying to continue...")
+        continue
+client.disconnect()
+print("Done. Message sent to all users.")
